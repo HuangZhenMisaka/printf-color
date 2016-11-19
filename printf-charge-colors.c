0281f77c5a0debc457733807f94b59411bc9034b
黄震 <hz15915536486@icloud.com>
2016-11-20 01:33:04 +0800

    the first commit about printf

---
+++ printf-charge-colors.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+	/* code */
+	int i;
+	
+	for (int i = 0; i < 30; ++i)
+	{
+		/* code */
+		for (int i = 41; i <48 ; ++i)
+	{
+		/* code */
+
+		printf(" \033[%dm_(:з」∠)_\033[0m",i);
+        printf(" \033[1B");  
+         
+        
+		
+	}
+	}
+	
+
+	return 0;
+}
